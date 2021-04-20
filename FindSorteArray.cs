75cf13ce7e484805883aac59648c850d842e21bf
ccesarrod <ccesarrod@gmail.com>
2021-04-20 08:15:23 -0400

    Update FindSorteArray.cs

--- FindSorteArray.cs
+++ FindSorteArray.cs
@@ -2,9 +2,9 @@
 {
     public  class SorteArray
     {
-        public int Find(int[] array, int to_search)
-        {
-           var middle = array.Length / 2;
-        }
+       //public int Find(int[] array, int to_search)
+        //{
+        //   var middle = array.Length / 2;
+        //}
     }
 }
